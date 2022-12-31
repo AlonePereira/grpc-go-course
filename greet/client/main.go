ed7ca25bcf7f09ba526a86a2b9f54def70465265
Alone Silva <alone.pereira@hotmail.com>
2022-12-31 05:26:43 -0300

    Create client

--- greet/client/main.go
+++ greet/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	pb "github.com/AlonePereira/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -18,4 +19,6 @@ func main() {
 
 	defer conn.Close()
 
+	c := pb.NewGreetServiceClient(conn)
+	doGreet(c)
 }
